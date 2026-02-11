1b601c207f7c904fd12e3c73cb550ff444689eb4
Yasser <yasseretudes004@gmail.com>
2026-02-11 15:48:13 +0100

    added APIGw

--- apiGW/src/main/java/com/parking/ApiGWApplication.java
+++ apiGW/src/main/java/com/parking/ApiGWApplication.java
@@ -1,13 +1,12 @@
 package com.parking;
 
-/**
- * Hello world!
- *
- */
-public class apiGWApplication
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGWApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGWApplication.class, args);
     }
 }
