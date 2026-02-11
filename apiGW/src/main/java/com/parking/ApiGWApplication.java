a3ec113fafdd852613fc4cd7634de687ee6bb901
Yasser <yasseretudes004@gmail.com>
2026-02-11 14:01:13 +0100

    added APIGw

---
+++ apiGW/src/main/java/com/parking/ApiGWApplication.java
@@ -0,0 +1,13 @@
+package com.parking;
+
+/**
+ * Hello world!
+ *
+ */
+public class apiGWApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
